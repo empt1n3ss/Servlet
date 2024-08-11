0c22f0ffe06da27611b1c34135d336bde08fa031
n38757902 <n38757902@gmail.com>
2024-08-11 21:26:27 +0300

    Initial commit

---
+++ src/main/java/org/example/controller/PostController.java
@@ -0,0 +1,56 @@
+package org.example.controller;
+
+import com.google.gson.Gson;
+import org.example.exception.NotFoundException;
+import org.example.model.Post;
+import org.example.service.PostService;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Reader;
+
+public class PostController {
+  public static final String APPLICATION_JSON = "application/json";
+  private static final Gson gson = new Gson();
+  private final PostService service;
+
+  public PostController(PostService service) {
+    this.service = service;
+  }
+
+  private void writeResponse(HttpServletResponse response, Object data) throws IOException {
+    response.setContentType(APPLICATION_JSON);
+    response.getWriter().print(gson.toJson(data));
+  }
+
+  public void all(HttpServletResponse response) throws IOException {
+    response.setContentType(APPLICATION_JSON);
+    final var data = service.all();
+    final var gson = new Gson();
+    response.getWriter().print(gson.toJson(data));
+  }
+
+  public void getById(long id, HttpServletResponse response) throws IOException {
+    try {
+      writeResponse(response, service.getById(id));
+    } catch (NotFoundException e) {
+      response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+      writeResponse(response, new NotFoundException("Post not found"));
+    }
+  }
+
+  public void save(Reader body, HttpServletResponse response) throws IOException{
+    Post post = gson.fromJson(body, Post.class);
+    writeResponse(response, service.save(post));
+  }
+
+  public void removeById(long id, HttpServletResponse response) throws IOException{
+    try {
+      service.removeById(id);
+      response.setStatus(HttpServletResponse.SC_NO_CONTENT);
+    } catch (NotFoundException e) {
+      response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+      writeResponse(response, new NotFoundException("Post not found"));
+    }
+  }
+}
---
+++ src/main/java/org/example/repository/PostRepository.java
@@ -0,0 +1,43 @@
+package org.example.repository;
+
+import org.example.exception.NotFoundException;
+import org.example.model.Post;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+// Stub
+public class PostRepository {
+  private final Map<Long, Post> posts = new ConcurrentHashMap<>();
+  private final AtomicLong idCounter = new AtomicLong();
+
+  public List<Post> all() {
+    return new ArrayList<>(posts.values());
+  }
+
+  public Optional<Post> getById(long id) {
+    return Optional.ofNullable(posts.get(id));
+  }
+
+  public Post save(Post post) {
+    if (post.getId() == 0) {
+      long id = idCounter.incrementAndGet();
+      post.setId(id);
+      posts.put(id, post);
+      return post;
+    } else {
+      if (posts.containsKey(post.getId())) {
+        posts.put(post.getId(), post);
+        return post;
+      } else {
+        throw new NotFoundException("Post not found");
+      }
+    }
+  }
+  public void removeById(long id) {
+    if (posts.remove(id) == null) {
+      throw new NotFoundException("Post not found");
+    }
+  }
+}
